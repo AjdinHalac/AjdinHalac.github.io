d4e437ceea883f0ed3d964f09909fb92312ea018
Ajdin Halac <ajdin.halac@hotmail.com>
2022-10-19 16:33:28 +0200

    feat(*): add PKCE grant type

--- index.js
+++ index.js
@@ -13,43 +13,50 @@ function parseJwt(token) {
   return JSON.parse(jsonPayload);
 }
 
-// document.getElementByClassName("page").each(function (i, e) {
-//   this.click(function (event) {
-//     let x = event.pageX;
-//     let y = event.pageY;
-
-//     let nextItem = i + 1;
-//     if (nextItem >= document.getElementByClassName("page").length) {
-//       nextItem = 0;
-//     }
-
-//     $(".page:eq(" + nextItem + ")").css(
-//       "z-index",
-//       parseInt(this.css("z-index")) + 1
-//     );
-//     $(".page:eq(" + nextItem + ")").css(
-//       "clip-path",
-//       "circle(0% at " + x + "px " + y + "px)"
-//     );
-
-//     anime({
-//       targets: document.getElementByClassName("page")[nextItem],
-//       update: function (anim) {
-//         $(".page:eq(" + nextItem + ")").css(
-//           "clip-path",
-//           "circle(" + anim.progress * 2 + "% at " + x + "px " + y + "px)"
-//         );
-//       },
-//     });
-//   });
-// });
-
-if (window.localStorage.getItem("biocerticaAuthorizationCode") !== null) {
+if (
+  window.localStorage.getItem("biocerticaIDToken") !== null &&
+  window.localStorage.getItem("biocerticaCodeVerifier") !== null
+) {
+  document.getElementById("page-choose").style.zIndex = 0;
+  document.getElementById("page-implicit-login").style.zIndex = 0;
+  document.getElementById("page-authorization-code-login").style.zIndex = 0;
+  document.getElementById("page-implicit-logged-in").style.zIndex = 0;
+  document.getElementById("page-authorization-logged-in").style.zIndex = 0;
+  document.getElementById("page-pkce-login").style.zIndex = 0;
+  document.getElementById("page-pkce-logged-in").style.zIndex = 1;
+  document.getElementById("btn-oauth").style.zIndex = 0;
+  const profile = parseJwt(localStorage.getItem("biocerticaIDToken"));
+  document.getElementById("verifier-pkce").innerText = localStorage.getItem(
+    "biocerticaCodeVerifier"
+  );
+  document.getElementById("firstName-pkce").innerText =
+    "First name: " + profile.first_name;
+  document.getElementById("lastName-pkce").innerText =
+    "Last name: " + profile.last_name;
+  document.getElementById("email-pkce").innerText = "Email: " + profile.email;
+  document.getElementById("phoneNumber-pkce").innerText =
+    "Phone number: " + profile.phone;
+  document.getElementById("accessToken-pkce").innerText =
+    "Access Token: " + localStorage.getItem("biocerticaAccessToken");
+  document.getElementById("refreshToken-pkce").innerText =
+    "Refresh Token: " + localStorage.getItem("biocerticaRefreshToken");
+  document
+    .getElementById("recommendation-pkce")
+    .addEventListener("click", async function () {
+      await bapi.getProducts();
+      document.getElementById("vitaminA-pkce").innerText =
+        "Product recommendation based on DNA Results: " +
+        JSON.stringify(window.bapi.products);
+    });
+} else if (
+  window.localStorage.getItem("biocerticaAuthorizationCode") !== null
+) {
   document.getElementById("page-choose").style.zIndex = 0;
   document.getElementById("page-implicit-login").style.zIndex = 0;
   document.getElementById("page-authorization-code-login").style.zIndex = 0;
   document.getElementById("page-implicit-logged-in").style.zIndex = 0;
   document.getElementById("page-authorization-logged-in").style.zIndex = 1;
+  document.getElementById("page-pkce-login").style.zIndex = 0;
   document.getElementById("btn-oauth").style.zIndex = 0;
   const code = localStorage.getItem("biocerticaAuthorizationCode");
   document.getElementById("code").innerText =
@@ -60,6 +67,8 @@ if (window.localStorage.getItem("biocerticaAuthorizationCode") !== null) {
   document.getElementById("page-authorization-code-login").style.zIndex = 0;
   document.getElementById("page-implicit-logged-in").style.zIndex = 1;
   document.getElementById("page-authorization-logged-in").style.zIndex = 0;
+  document.getElementById("page-pkce-login").style.zIndex = 0;
+  document.getElementById("page-pkce-logged-in").style.zIndex = 0;
   document.getElementById("btn-oauth").style.zIndex = 0;
   const profile = parseJwt(localStorage.getItem("biocerticaIDToken"));
   document.getElementById("firstName").innerText =
@@ -87,6 +96,8 @@ if (window.localStorage.getItem("biocerticaAuthorizationCode") !== null) {
   document.getElementById("page-authorization-code-login").style.zIndex = 0;
   document.getElementById("page-implicit-logged-in").style.zIndex = 0;
   document.getElementById("page-authorization-logged-in").style.zIndex = 0;
+  document.getElementById("page-pkce-login").style.zIndex = 0;
+  document.getElementById("page-pkce-logged-in").style.zIndex = 0;
   document.getElementById("btn-oauth").style.zIndex = 0;
 }
 
@@ -104,6 +115,8 @@ document
     document.getElementById("page-authorization-code-login").style.zIndex = 0;
     document.getElementById("page-implicit-logged-in").style.zIndex = 0;
     document.getElementById("page-authorization-logged-in").style.zIndex = 0;
+    document.getElementById("page-pkce-login").style.zIndex = 0;
+    document.getElementById("page-pkce-logged-in").style.zIndex = 0;
     document.getElementById("btn-oauth").style.zIndex = 2;
   });
 
@@ -121,6 +134,27 @@ document
     document.getElementById("page-authorization-code-login").style.zIndex = 1;
     document.getElementById("page-implicit-logged-in").style.zIndex = 0;
     document.getElementById("page-authorization-logged-in").style.zIndex = 0;
+    document.getElementById("page-pkce-login").style.zIndex = 0;
+    document.getElementById("page-pkce-logged-in").style.zIndex = 0;
+    document.getElementById("btn-oauth").style.zIndex = 2;
+  });
+
+document
+  .getElementById("btn-pkce")
+  .addEventListener("click", async function () {
+    bapi.init(
+      "d916027f-2f42-4b38-b2aa-b9eecb1dd50d",
+      window.location.href.split("?")[0],
+      "pkce",
+      "code"
+    );
+    document.getElementById("page-choose").style.zIndex = 0;
+    document.getElementById("page-implicit-login").style.zIndex = 0;
+    document.getElementById("page-authorization-code-login").style.zIndex = 0;
+    document.getElementById("page-implicit-logged-in").style.zIndex = 0;
+    document.getElementById("page-authorization-logged-in").style.zIndex = 0;
+    document.getElementById("page-pkce-login").style.zIndex = 1;
+    document.getElementById("page-pkce-logged-in").style.zIndex = 0;
     document.getElementById("btn-oauth").style.zIndex = 2;
   });
 
@@ -132,6 +166,8 @@ document
     document.getElementById("page-authorization-code-login").style.zIndex = 0;
     document.getElementById("page-implicit-logged-in").style.zIndex = 0;
     document.getElementById("page-authorization-logged-in").style.zIndex = 0;
+    document.getElementById("page-pkce-login").style.zIndex = 0;
+    document.getElementById("page-pkce-logged-in").style.zIndex = 0;
     document.getElementById("btn-oauth").style.zIndex = 0;
     localStorage.clear();
   });
@@ -144,6 +180,22 @@ document
     document.getElementById("page-authorization-code-login").style.zIndex = 0;
     document.getElementById("page-implicit-logged-in").style.zIndex = 0;
     document.getElementById("page-authorization-logged-in").style.zIndex = 0;
+    document.getElementById("page-pkce-login").style.zIndex = 0;
+    document.getElementById("page-pkce-logged-in").style.zIndex = 0;
+    document.getElementById("btn-oauth").style.zIndex = 0;
+    localStorage.clear();
+  });
+
+document
+  .getElementById("btn-logout-pkce")
+  .addEventListener("click", async function () {
+    document.getElementById("page-choose").style.zIndex = 1;
+    document.getElementById("page-implicit-login").style.zIndex = 0;
+    document.getElementById("page-authorization-code-login").style.zIndex = 0;
+    document.getElementById("page-implicit-logged-in").style.zIndex = 0;
+    document.getElementById("page-authorization-logged-in").style.zIndex = 0;
+    document.getElementById("page-pkce-login").style.zIndex = 0;
+    document.getElementById("page-pkce-logged-in").style.zIndex = 0;
     document.getElementById("btn-oauth").style.zIndex = 0;
     localStorage.clear();
   });
